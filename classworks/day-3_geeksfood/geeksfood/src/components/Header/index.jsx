31fa0e87dbec624e2fc8051d51620b557b951d34
Anupam Yadav <anupamy571@gmail.com>
2024-05-06 22:29:54 +0530

    updating some features

--- classworks/day-3_geeksfood/geeksfood/src/components/Header/index.jsx
+++ classworks/day-3_geeksfood/geeksfood/src/components/Header/index.jsx
@@ -3,7 +3,29 @@ import styles from "./styles.module.css";
 import logoImage from "../images/logo.svg";
 
 const Header = () => {
-  const navLinks = ["Home", "Quote", "Resturants", "Foods", "Contact"];
+  // const navLinks = ["Home", "Quote", "Resturants", "Foods", "Contact"];
+  const navLinks = [
+    {
+      name: "Home",
+      link: "#",
+    },
+    {
+      name: "Quote",
+      link: "../Quote/index.jsx",
+    },
+    {
+      name: "Resturants",
+      link: "https://www.geeksforgeeks.org/",
+    },
+    {
+      name: "Foods",
+      link: "https://www.geeksforgeeks.org/",
+    },
+    {
+      name: "Contact",
+      link: "https://www.geeksforgeeks.org/",
+    },
+  ];
 
   return (
     <div className="header">
@@ -16,7 +38,9 @@ const Header = () => {
         <div>
           <ul>
             {navLinks.map((link, index) => (
-              <li key={index}>{link}</li>
+              <a key={index} href={link.link}>
+                {link.name}
+              </a>
             ))}
           </ul>
         </div>
---
+++ classworks/day-3_geeksfood/geeksfood/src/components/Quotes/index.jsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <h1>Quotes</h1>
+    </div>
+  );
+};
+
+export default index;
